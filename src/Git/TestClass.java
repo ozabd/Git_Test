5926e15049c4148490e1aa258c6c6cd2997c66bc
ozabd <69389015+ozabd@users.noreply.github.com>
2020-08-08 15:09:10 +0200

    Update TestClass.java

--- src/Git/TestClass.java
+++ src/Git/TestClass.java
@@ -3,5 +3,6 @@ package Git;
 public class TestClass {
     public static void main(String[] args){
         System.out.println("Git den merhaba");
+        System.out.println("Git Hub'dan merhaba");
     }
 }
